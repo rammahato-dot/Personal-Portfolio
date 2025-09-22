f60615e3c21052347416f654e721441948fb9e65
rammahato-dot <rammahato6655@gmail.com>
2025-09-22 22:09:27 +0545

    Build first version of personal portfolio: dark theme & interactive sections

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typed.js setup
+var typed = new Typed("#element", {
+  strings: ["Web Developer", "Web Designer", "Video Editor", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true,
+});
